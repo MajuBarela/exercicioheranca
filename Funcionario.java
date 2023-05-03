cd2a722ef4926dbd427877cea176c195d9fe2b32
MajuBarela <maria.netto01@etec.sp.gov.br>
2023-05-03 17:08:14 +0000

    Salvando exercício

---
+++ Funcionario.java
@@ -0,0 +1,21 @@
+public class Funcionario extends Pessoa {
+
+    private double salario;
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public Funcionario (String nome, int idade, String endereco, double salario){
+        super(nome, idade, endereco);
+        setSalario(salario);
+    }
+
+    public Funcionario(){
+        
+    }   
+}
